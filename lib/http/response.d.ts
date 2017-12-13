5af6e5ad4cba92840c76115c21221f8201c96a65
Dominik Kundel <dominik.kundel@gmail.com>
2017-12-13 14:58:06 +0100

    Add initial types for base/http folders

--- lib/http/response.d.ts
+++ lib/http/response.d.ts
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 declare class Response<TPayload> {
+=======
+declare class Reponse<TPayload> {
+>>>>>>> Add initial types for base/http folders
   constructor(statusCode: number, body: TPayload);
   toString(): string;
 }
